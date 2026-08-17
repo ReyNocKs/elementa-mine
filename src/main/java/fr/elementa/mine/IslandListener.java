741c3b343b775907f564b1a5bef58c9e7676fedf
ReyNocKs <reynockss@gmail.com>
2026-08-17 07:41:47 +0200

    Add files via upload

--- src/main/java/fr/elementa/mine/IslandListener.java
+++ src/main/java/fr/elementa/mine/IslandListener.java
@@ -2,26 +2,37 @@ package fr.elementa.mine;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import world.bentobox.bentobox.api.events.island.IslandEvent;
 import world.bentobox.bentobox.database.objects.Island;
 
-public class IslandListener implements org.bukkit.event.Listener {
+/** Creates the default mine when BentoBox reports that an island was created. */
+public final class IslandListener implements Listener {
+    private static final int MINE_MIN_Y = 5;
+    private static final int MINE_MAX_Y = 40;
 
     private final ElementaMine plugin;
     private final MineManager mineManager;
-
-    // Bande de hauteur reservee a la mine (zone habituellement vide sous une ile BSkyBlock)
-    private static final int MINE_MIN_Y = 5;
-    private static final int MINE_MAX_Y = 40;
 
     public IslandListener(ElementaMine plugin, MineManager mineManager) {
         this.plugin = plugin;
         this.mineManager = mineManager;
     }
 
-    @org.bukkit.event.EventHandler
-    public void onIslandCreated(IslandEvent.IslandCreatedEvent event) {
-        createMineForIsland(event.getIsland());
+    @EventHandler
+    public void onIslandEvent(IslandEvent event) {
+        if (event == null || event.getReason() != IslandEvent.Reason.CREATED) {
+            return;
+        }
+        try {
+            Island island = event.getIsland();
+            if (island != null) {
+                createMineForIsland(island);
+            }
+        } catch (RuntimeException ex) {
+            plugin.getLogger().warning("Impossible de creer la mine BentoBox: " + ex.getMessage());
+        }
     }
 
     public static String regionNameFor(Island island) {
@@ -29,30 +40,36 @@ public class IslandListener implements org.bukkit.event.Listener {
     }
 
     private void createMineForIsland(Island island) {
-        World world = island.getWorld();
-        if (world == null || island.getCenter() == null) return;
+        try {
+            Location center = island.getCenter();
+            World world = island.getWorld();
+            if (center == null || world == null) {
+                return;
+            }
 
-        String regionName = regionNameFor(island);
+            String regionName = regionNameFor(island);
+            int range = island.getProtectionRange();
+            Location pos1 = new Location(world, center.getBlockX() - range, MINE_MIN_Y,
+                    center.getBlockZ() - range);
+            Location pos2 = new Location(world, center.getBlockX() + range, MINE_MAX_Y,
+                    center.getBlockZ() + range);
 
-        int centerX = island.getCenter().getBlockX();
-        int centerZ = island.getCenter().getBlockZ();
-        int range = island.getProtectionRange();
+            MineRegion region = mineManager.getOrCreate(regionName);
+            region.setPos1(pos1);
+            region.setPos2(pos2);
+            region.setTier(1);
 
-        Location pos1 = new Location(world, centerX - range, MINE_MIN_Y, centerZ - range);
-        Location pos2 = new Location(world, centerX + range, MINE_MAX_Y, centerZ + range);
+            MineTiers.TierData tierData = MineTiers.get(1);
+            region.setResetThreshold(tierData.resetThreshold);
+            region.setComposition(tierData.composition);
+            mineManager.generate(region);
+            mineManager.saveAll();
 
-        MineRegion region = mineManager.getOrCreate(regionName);
-        region.setPos1(pos1);
-        region.setPos2(pos2);
-        region.setTier(1);
-
-        MineTiers.TierData tierData = MineTiers.get(1);
-        region.setResetThreshold(tierData.resetThreshold);
-        region.setComposition(tierData.composition);
-
-        mineManager.generate(region);
-        mineManager.saveAll();
-
-        plugin.getLogger().info("Mine T1 (" + tierData.name + ") generee automatiquement pour l'ile " + island.getUniqueId());
+            plugin.getLogger().info("Mine T1 (" + tierData.name
+                    + ") generee automatiquement pour l'ile " + island.getUniqueId());
+        } catch (RuntimeException ex) {
+            plugin.getLogger().warning("Erreur pendant la generation de la mine BentoBox: "
+                    + ex.getMessage());
+        }
     }
 }
